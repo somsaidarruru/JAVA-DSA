3fdd4f5eeca66d05c52e0895f3ba0192e25765fa
somsai359 <somasaichowdary01@gmail.com>
2026-08-16 02:16:31 +0530

    day13 non repeating

--- Nonrepeating.java
+++ Nonrepeating.java
@@ -6,6 +6,7 @@ class Nonrepeating {
 
         Map<Integer,Integer> count = new HashMap<>();
 
+
         for(int i = 0 ; i < arr.length ; i++){
 
          count.put(arr[i], count.getOrDefault(arr[i], 0)+1);
@@ -19,6 +20,7 @@ class Nonrepeating {
                 System.out.println(arr[i]);
 
                 break;
+                
             }
         }
     }
